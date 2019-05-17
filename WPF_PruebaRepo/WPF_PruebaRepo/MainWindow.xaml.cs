ac8eed7d6b8106a1df2a8e58d1fafadb0c634d89
Juan Manuel Benitez Carballo <manuel_carballo95@hotmail.com>
2019-05-17 13:48:11 -0400

    Prueba cargando una imagen

--- WPF_PruebaRepo/WPF_PruebaRepo/MainWindow.xaml.cs
+++ WPF_PruebaRepo/WPF_PruebaRepo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,19 @@ namespace WPF_PruebaRepo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
         }
     }
 }
